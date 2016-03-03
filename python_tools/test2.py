259ee5a14130d14ad40f986743bac427ded2adbc
Nacturne <xliangs1122@gmail.com>
2016-03-03 11:41:58 -0500

    updated

--- python_tools/test2.py
+++ python_tools/test2.py
@@ -1,14 +1,12 @@
-file_in = open('input/original/test.txt', 'r')
+from Core import TreeClass as tc
 
-total = 0
-neutral = 0
+file_in = open('input/original/dev.txt', 'r')
+file_out = open('output/non_neutral_dev.txt','w')
+
 for line in file_in:
-    if line[0] == '(':
-        total += 1
-        if line[1] == '2':
-            neutral += 1
+    if not line[1] == '2':
+        file_out.write(line)
 
-print('total: {0}'.format(total))
-print('neutral: {0}'.format(neutral))
-print('portion: {0}'.format(neutral/float(total)))
+file_out.close()
 file_in.close()
+
